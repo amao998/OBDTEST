ce9f9bdb93efd01aa3eebb8ead30f9008665508d
amao <285515535@qq.com>
2019-07-18 18:02:45 +0800

    第4次提交

--- app/src/main/java/com/mh/obdtest/obdtest/MyPopupWindow.java
+++ app/src/main/java/com/mh/obdtest/obdtest/MyPopupWindow.java
@@ -20,7 +20,7 @@ public class MyPopupWindow extends PopupWindow{
         this.setFocusable(true);
         this.setAnimationStyle(R.style.dialog_style);
 
-        String VER="3";
+        String VER="4";
 
     }
 }
